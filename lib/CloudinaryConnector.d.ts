17497cdcd0f4ecfd5e79df9018c1f7f64f76136a
David Seibert <david@seibert.io>
2019-05-06 14:45:54 +0200

    refactor: converted to typescript

---
+++ lib/CloudinaryConnector.d.ts
@@ -0,0 +1,67 @@
+interface BaseConfig {
+    type?: 'fetch' | 'upload';
+    secure?: boolean;
+}
+interface BreakpointConfig {
+    minWidth: number;
+    maxWidth: number;
+    minBreakpointSizeDiffKB: number;
+    maxBreakpoints: number;
+    list?: any[];
+}
+interface Breakpoint {
+    src: string;
+    width: number;
+    height?: number;
+}
+export default class CloudinaryConnector {
+    private static BASE_CONFIG_DEFAULTS;
+    private static BREAKPOINT_CONFIG_DEFAULTS;
+    private static TRANSFORMATION_DEFAULTS;
+    static MAX_PIXEL: number;
+    private _baseConfig;
+    private _breakpointConfig;
+    private _transformationDefaults;
+    constructor(cloudName: string, baseConfig?: BaseConfig);
+    /**
+     *
+     * @param {Object} config
+     */
+    updateBaseConfig(config: BaseConfig): void;
+    /**
+     *
+     * @returns {{type: string, secure: boolean, baseConfig}|*}
+     */
+    getBaseConfig(): BaseConfig;
+    /**
+     *
+     * @param {Object} config
+     */
+    updateBreakpointConfig(config: BreakpointConfig): void;
+    /**
+     *
+     * @returns {{minBreakpointSizeDiffKB: number, maxBreakpoints: number, minWidth: number, maxWidth: number}|*}
+     */
+    getBreakpointConfig(): BreakpointConfig;
+    /**
+     *
+     * @param {Object} defaults
+     */
+    updateTransformationDefaults(defaults: Record<string, any>): void;
+    /**
+     *
+     * @param {String} imageUrl
+     * @param {Object|Array} options
+     * @returns {Promise<{src: *, width: *, height: *}[]>}
+     */
+    getSrcSet(imageUrl: string, options?: Record<string, any>): Promise<Breakpoint[]>;
+    /**
+     *
+     * @param {String} imageUrl
+     * @param {Array} transformation
+     * @param {Object} [breakpointConfig]
+     * @returns {Promise<Array>}
+     */
+    private getBreakpoints;
+}
+export {};
